9df04cf2fc22cb619e1ad2afeb6c8510f945467c
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2026-06-23 21:12:29 +0530

    Day-112

---
+++ Day-111/day.111.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int mn = 1e9, mx = -1;
+
+        for (int i = 0; i < n; i++) {
+            int h;
+            cin >> h;
+            mn = min(mn, h);
+            mx = max(mx, h);
+        }
+
+        cout << mx - mn + 1 << '\n';
+    }
+
+    return 0;
+}

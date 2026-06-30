390362077da50b3e40fa101a81c6c81dfac9cd06
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2026-06-30 21:43:33 +0530

    Day 124

---
+++ Day-124/day.124.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long n, k;
+        cin >> n >> k;
+
+        long long ans = 0;
+
+        for (long long cost = 1; cost <= n; cost *= 2) {
+            long long take = min(k, n / cost);
+
+            ans += take;
+            n -= take * cost;
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}

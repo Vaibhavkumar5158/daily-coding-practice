6a2689ca184a007867bc14306b8283ddab658759
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2026-07-17 21:57:51 +0530

    Day 137

---
+++ Day 137/Day 137.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x;
+        cin >> x;
+
+        if (x == 67)
+            cout << 67 << "\n";
+        else
+            cout << x + 1 << "\n";
+    }
+
+    return 0;
+}

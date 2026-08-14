20dbab2ceef9a18f25e1e3f0aed657caca7f050e
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2026-08-14 23:26:11 +0530

    Day 155

---
+++ Day 155/Day 155.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int x) {
+    if (x < 2)
+        return false;
+
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (isPrime(n + 1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
